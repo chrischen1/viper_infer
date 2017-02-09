cb293ba45844f6c9bdd0c69ba05b0eb8d2c36f38
crestfall1 <chen08900@gmail.com>
2017-02-09 10:33:36 -0500

    fix length issue

--- id2symbol.R
+++ id2symbol.R
@@ -1,7 +1,7 @@
 # transform all entrez id in regulon object to gene symbol
 library(biomaRt)
 # object is downloaded from https://figshare.com/articles/Human_breast_carcinoma_signalome_regulatory_network/695962
-load('brca_tcga_rnaseq851_signalomeregulon.rda')
+load('~/Dropbox/_ChrisProject/workspace/viper/regulons/brca_tcga_rnaseq851_signalomeregulon.rda')
 all_genes <- unique(c(names(regul),unlist(lapply(regul,function(x)names(x$tfmode))),unlist(lapply(regul,function(x)names(x$likelihood)))))
 
 #mapping, only genes with 1:1 mapping are kept
@@ -13,11 +13,13 @@ names(genes) <- results$entrezgene
 
 #change gene names in regulon object
 change_symbol <- function(x){
-  x$tfmode <- x$tfmode[names(x$tfmode) %in% names(genes)]
-  x$likelihood <- x$likelihood[names(x$tfmode) %in% names(genes)]
+  ind_keep <- names(x$tfmode) %in% names(genes)
+  x$tfmode <- x$tfmode[ind_keep]
+  x$likelihood <- x$likelihood[ind_keep]
   names(x$tfmode) <- genes[names(x$tfmode)]
   x
 }
+
 regul_symbol <- regul[names(regul) %in% names(genes)]
 names(regul_symbol) <- genes[names(regul_symbol)]
 regul_symbol <- lapply(regul_symbol,change_symbol)
